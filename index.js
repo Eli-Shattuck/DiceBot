c5dc19837e0ac07917303dead8bfa0f59011218c
tim-schafer <99783395+tim-schafer@users.noreply.github.com>
2022-02-15 23:54:35 -0500

    initial commit of dicebot v3
    Based on dicebot v2 by Eli-Shattuck

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require("dotenv").config();
+const Parser = require('./parser_classes/parser.js');
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    let p = new Parser(msg);
+    p.parse();
+    //msg.reply('Hello world!');
+})
+
+client.login(process.env.BOT_TOKEN);
